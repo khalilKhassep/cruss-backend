b43ffe1bd95f16fa28a25be3112bd0ded9269859
khalilKhassep <khalil.code.source@gmail.com>
2021-06-03 11:08:16 +0300

    Addmin css

--- app/views/admin/Layout.jsx
+++ app/views/admin/Layout.jsx
@@ -14,7 +14,7 @@ function Layout(props) {
                     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous"></link>
-                    <link rel="stylesheet" href="http://localhost:3333/css/main.css" />
+                    <link rel="stylesheet" href="https://cruss-backend.herokuapp.com/css/main.css" />
                     <title>Document</title>
                 </head>
                 <body className={'nav-md'}>
